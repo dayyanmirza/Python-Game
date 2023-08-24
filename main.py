86a842f6518d0306c9191b04f301de070aa37c4c
dayyanmirza <dayyanmirza15@gmail.com>
2023-08-24 12:52:19 +0100

    GUI practice

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import tkinter as tk
 
 window = tk.Tk()
-saying = tk.Label(text="Python rocks!")
-saying.pack()
+
+label = tk.Label(
+    text="Hello, Tkinter!",
+    fg="red",
+    bg="#34A2FE",
+    width=40,
+    height=20
+    )
+label.pack() # adds label widget to window
+
+button = tk.Button(
+    text="Click me!",
+    width=25, 
+    height=5,
+    background="blue",
+    foreground="red"
+) 
+button.pack() # adds button widget to window
+
 window.mainloop()
+
