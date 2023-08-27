86cc268faccbc5d30077d0f6c76f4ba987b2924c
dayyanmirza <dayyanmirza15@gmail.com>
2023-08-27 11:53:34 +0100

    created the basic class structure

--- classes.py
+++ classes.py
@@ -55,12 +55,78 @@ Peru:
 
 
 Classes:
-- Engine 
-- Scenes
-    - Home 
-    - Phonebooth
-    - Plane 
-    - Ecuador
-    - Peru
+* Map 
+    - next_scene
+    - opening_scene 
+* Engine 
+    - play
+* Scene
+    - enter
+    * Death
+    * Home 
+    * Phonebooth
+    * Plane 
+    * Ecuador
+    * Peru
 
 """
+
+class Scene(object):
+
+    def enter(self): # make a generic thing here that can be inherited by all scenes. i.e. prints out the scene or location name.
+        pass
+
+class Engine(object):
+
+    def __init__(self, scene_map):
+        pass
+
+    def play(self):
+        pass
+
+class Death(Scene):
+
+    def enter(self):
+        pass
+
+class Home(Scene):
+
+    def enter(self):
+        pass
+
+class Phonebooth(Scene):
+
+    def enter(self):
+        pass
+
+class Plane(Scene):
+
+    def enter(self):
+        pass
+
+class Ecuador(Scene):
+
+    def enter(self):
+        pass
+
+class Peru(Scene):
+
+    def enter(self):
+        pass
+
+class Map(object):
+
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
+
+
+a_map = Map('home')
+a_game = Engine(a_map)
+a_game.play()
+        
